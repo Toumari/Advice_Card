06e2c0f57fb914d832e9806173f1c78d325f19ad
Jack Williams <jack.w141@outlook.com>
2024-07-20 20:48:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const quoteText = document.querySelector(".quote");
+const quoteButton = document.querySelector(".quote-btn");
+const advice = document.querySelector("small");
+
+quoteButton.addEventListener("click", async () => {
+  const quote = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-cache",
+  });
+
+  const quoteData = await quote.json();
+  console.log(quoteData);
+  quoteText.textContent = quoteData.slip.advice;
+  advice.textContent = `Advice #${quoteData.slip.id}`;
+});
